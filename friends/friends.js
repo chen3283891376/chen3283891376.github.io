dfc953c798d91f68eacbd07e7621442f486f63e5
chen3283891376 <3283891376@qq.com>
2025-06-30 17:08:36 +0800

    Site updated: 2025-06-30 17:08:33

---
+++ friends/friends.js
@@ -0,0 +1,21 @@
+const friendsData = {
+  friends: [
+    {
+      class: {
+        name: '小伙伴们',
+        desc: '没啥好嗦的。',
+        random: true
+      },
+      list: [
+        {
+            name: 'zhz1024',
+            url: 'https://littlefish.netlify.app/',
+            avatar: 'https://img.notionusercontent.com/s3/prod-files-secure%2Fbdef698d-a65e-45a6-a22c-ed6646fba6dd%2Fcb99bc1c-ce5c-40ae-8dcb-3a590d6ee409%2Fb_1bed54cd1a07039dbf2d0f278dd9c191.jpg/size/?exp=1751360176&sig=NUiPo00ZV0pWntMbeqaZsqcMS0THLHv_nd2Dw_3Aywk&id=1aaed04b-c190-811f-879e-000bc6223c7c&table=collection',
+            desc: '一只Py，Go，Next.js，ReactJs，部署运维通吃的Fish，是个糕手。'
+        }
+      ]
+    },
+  ]
+};
+
+export default friendsData;
